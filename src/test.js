684624b0f483d2522b4e79ebefa3e5fa9f4d37e5
Max Ghenis <mghenis@gmail.com>
2025-02-03 21:38:11 -0500

    trivial change

--- src/test.js
+++ src/test.js
@@ -1,7 +1,7 @@
 function testMarkdownExport() {
   const markdown = convertToMarkdownWithComments();
-  Logger.log('Generated markdown length:', markdown.length);
-  Logger.log('\nMarkdown content:');
+  Logger.log('Generated markdown length: ', markdown.length);
+  Logger.log('\nMarkdown content: ');
   Logger.log(markdown);
   return markdown;
 }
